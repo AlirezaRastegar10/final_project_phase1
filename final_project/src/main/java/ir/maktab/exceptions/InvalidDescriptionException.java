778f2f58940ba098a936a4f5bd3ced769cc05cea
phylix <alirezarastegar117390@gmail.com>
2023-01-26 15:16:07 +0330

    46 --> Creating InvalidDescriptionException class for handling invalid description exception.

---
+++ final_project/src/main/java/ir/maktab/exceptions/InvalidDescriptionException.java
@@ -0,0 +1,8 @@
+package ir.maktab.exceptions;
+
+public class InvalidDescriptionException extends RuntimeException {
+
+    public InvalidDescriptionException(String message) {
+        super(message);
+    }
+}
