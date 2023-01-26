b261a0849ecbd304d3d2c1acb9b37657ec771a6f
phylix <alirezarastegar117390@gmail.com>
2023-01-26 15:17:17 +0330

    47 --> Creating InvalidEmailException class for handling invalid email pattern exception.

---
+++ final_project/src/main/java/ir/maktab/exceptions/InvalidEmailException.java
@@ -0,0 +1,8 @@
+package ir.maktab.exceptions;
+
+public class InvalidEmailException extends RuntimeException {
+
+    public InvalidEmailException(String message) {
+        super(message);
+    }
+}
