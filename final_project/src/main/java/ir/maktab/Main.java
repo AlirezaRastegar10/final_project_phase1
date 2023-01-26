68369b97f2ca6858902269f226b660e080c011f1
phylix <alirezarastegar117390@gmail.com>
2023-01-26 15:26:01 +0330

    58 --> Creating Main class for run project in my computer.

---
+++ final_project/src/main/java/ir/maktab/Main.java
@@ -0,0 +1,10 @@
+package ir.maktab;
+
+import ir.maktab.menu.MainMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.run();
+    }
+}
