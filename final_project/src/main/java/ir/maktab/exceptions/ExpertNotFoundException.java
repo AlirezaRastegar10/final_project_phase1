e19c59a455d1c9d5698b5308557c5611fb35499d
phylix <alirezarastegar117390@gmail.com>
2023-01-26 15:11:04 +0330

    42 --> Creating ExpertNotFoundException class for handling not found expert.

---
+++ final_project/src/main/java/ir/maktab/exceptions/ExpertNotFoundException.java
@@ -0,0 +1,7 @@
+package ir.maktab.exceptions;
+
+public class ExpertNotFoundException extends RuntimeException {
+    public ExpertNotFoundException(String message) {
+        super(message);
+    }
+}
