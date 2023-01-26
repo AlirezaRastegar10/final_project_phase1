08a827ad5a9f60e41be4b1890a0f7e7a4d66f82b
phylix <alirezarastegar117390@gmail.com>
2023-01-26 15:11:42 +0330

    43 --> Creating FileNotFoundException class for handling not found file with entry url.

---
+++ final_project/src/main/java/ir/maktab/exceptions/FileNotFoundException.java
@@ -0,0 +1,8 @@
+package ir.maktab.exceptions;
+
+public class FileNotFoundException extends RuntimeException {
+
+    public FileNotFoundException(String message) {
+        super(message);
+    }
+}
